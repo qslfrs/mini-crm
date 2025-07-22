dcf379af4403bc0529adbc35234d821b674129ce
QaisAlfarisi <qaisalfarisi@gmail.com>
2025-07-22 22:04:10 +0700

    perbaikan migrasi data

--- mini-crm-backend/src/app.ts
+++ mini-crm-backend/src/app.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import customerRoutes from './routes/customerRoutes';
 import orderRoutes from './routes/orderRoutes';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use('/customers', customerRoutes);
 app.use('/orders', orderRoutes);
 
